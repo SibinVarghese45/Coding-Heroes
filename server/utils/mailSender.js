d7702b5458dba627051e852d9aa185e9a78ead2b
SibinVarghese45 <sibinsunilvarghese@gmail.com>
2026-04-13 01:06:34 +0530

    Additional Changes

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,6 +4,8 @@ const mailSender = async (email, title, body) => {
     try{
             let transporter = nodemailer.createTransport({
                 host:process.env.MAIL_HOST,
+                port: 587, // or 465
+                secure: false, // true for 465, false for 587
                 auth:{
                     user: process.env.MAIL_USER,
                     pass: process.env.MAIL_PASS,
